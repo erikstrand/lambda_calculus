29bb0ddbd72a01f1b8b010d6579f0efa1fc0bac5
Erik Strand <erik.strand@cba.mit.edu>
2022-05-08 15:07:28 -0400

    Add more convenient printing helpers

--- lambda_calculus/term_serialization.h
+++ lambda_calculus/term_serialization.h
@@ -10,6 +10,23 @@ namespace lambda {
 // Assumes the stream expects UTF-8 text.
 void serialize_term(TermArena const& arena, TermId root, std::ostream& stream);
 
+//--------------------------------------------------------------------------------------------------
+struct TermPrinter {
+    TermArena const& arena;
+    TermId root;
+};
+
+//--------------------------------------------------------------------------------------------------
+inline std::ostream& operator<<(std::ostream& stream, TermPrinter const& printer) {
+    serialize_term(printer.arena, printer.root, stream);
+    return stream;
+}
+
+//--------------------------------------------------------------------------------------------------
+inline std::ostream& operator<<(std::ostream& stream, TermId term_id) {
+    return stream << term_id.value();
+}
+
 }
 
 #endif
